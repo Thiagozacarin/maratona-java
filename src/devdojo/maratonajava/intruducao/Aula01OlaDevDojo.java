adaca6f2f6676a8311a12fc58fdf9b1df945ed78
Thiago Zacarin <thiago.zacariin@hotmail.com>
2026-04-16 15:54:05 -0300

    Tipos Primitivos pt04

---
+++ src/devdojo/maratonajava/intruducao/Aula01OlaDevDojo.java
@@ -0,0 +1,15 @@
+package devdojo.maratonajava.intruducao;
+
+public class Aula01OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multíplas
+        linhas
+     */
+    public static void main (String[] args) {
+        System.out.println("Olá, Mundo!");
+
+
+    }
+}
---
+++ src/devdojo/maratonajava/intruducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package devdojo.maratonajava.intruducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+
+        System.out.println("The yers old is "+age+" age");
+        System.out.println(false);
+        System.out.println("char "+caractere);
+
+
+    }
+}
