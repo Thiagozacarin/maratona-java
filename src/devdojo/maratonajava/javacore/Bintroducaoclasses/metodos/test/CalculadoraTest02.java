f9b72277bb4d17b6071511297f8ec5be488335b3
Thiago Zacarin <thiago.zacariin@hotmail.com>
2026-04-24 22:57:44 -0300

    Métodospt03 - parametros,retorno

---
+++ src/devdojo/maratonajava/javacore/Bintroducaoclasses/metodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package devdojo.maratonajava.javacore.Bintroducaoclasses.metodos.test;
+
+import devdojo.maratonajava.javacore.Bintroducaoclasses.metodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(2, 10.5F);
+
+    }
+}
