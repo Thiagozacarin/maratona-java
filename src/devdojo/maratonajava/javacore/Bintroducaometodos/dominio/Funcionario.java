bc2144e9814f3bcddd56389af9604e48e5a6739d
Thiago Zacarin <thiago.zacariin@hotmail.com>
2026-04-26 21:39:24 -0300

    exer

--- src/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,14 +1,31 @@
 package devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
+    public String nome;
     public int idade;
+    public double[] salarios;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for(double salario: salarios){
+            System.out.print(salario + " ");
+        }
+        imprimeMedia();
     }
 
-
-    /*public int mediaDosSalarios() {
-        int soma = salario1 + salario2 + salario3;
-        System.out.println(mediaDosSalarios());
+    public void imprimeMedia(){
+        double media = 0;
+        for (double salario: salarios){
+            media += salario;
+        }
+         media /= salarios.length;
+        System.out.println("\nMédia salarial " + media);
     }
 }
 
-}
+
+
+
+
+
--- src/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,19 +5,10 @@ import devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        //funcionario.mediaDosSalarios();
         funcionario.nome = "Thiago Z";
         funcionario.idade = 24;
-        funcionario.salario1 = 5000;
-        funcionario.salario2 = 12000;
-        funcionario.salario3 = 18000;
+        funcionario.salarios = new double[] {5000, 12000, 18000};
 
-        System.out.println("Nome do funcionario : "+funcionario.nome);
-        System.out.println("Idade do funcionario : "+funcionario.idade);
-        System.out.println("Sálario 1 do funcionario : "+funcionario.salario1);
-        System.out.println("Sálario 2 do funcionario : "+funcionario.salario2);
-        System.out.println("Sálario 3 do funcionario : "+funcionario.salario3);
-
-        //System.out.println(funcionario.mediaDosSalarios());
+        funcionario.imprime();
     }
 }
